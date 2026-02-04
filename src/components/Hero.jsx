d9998286db75eeb073be14e2215b7adbacd26723
Tanisha Dey <deytanisha506@gmail.com>
2026-02-04 22:34:53 +0530

    hello

---
+++ src/components/Hero.jsx
@@ -0,0 +1,32 @@
+const HeroSection = () => {
+    return (
+        <main className="hero container">
+            <div className="hero-content">
+                <h1>YOUR FEET DESERVES THE BEST</h1>
+                <p>
+                    YOUR FEET DESERVES THE BEST AND WE’RE HERE TO HELP YOU WITH OUR 
+                    SHOES.YOUR FEET DESERVES THE BEST AND WE’RE HERE TO HELP YOU WITH OUR 
+                    SHOES.
+                </p>
+           
+            <div className="hero-btn">
+                <button>Shop Now</button>
+                <button className="category-btn">Category</button>
+            </div>
+
+            <div className="shopping">
+                <p>Also Available on</p>
+
+                <div className="brand-icons">
+                    <img src="/images/amazon.png" alt="amazon-logo" />
+                    <img src="/images/flipkart.png" alt="flipkart-logo" />
+                </div>
+            </div>
+            </div>
+            <div className="hero-image">
+                <img src="/images/shoe_image.png" alt="flipkart-logo" />
+            </div>
+        </main>
+        );
+};
+export default HeroSection; 
---
+++ src/components/Navigation.jsx
@@ -0,0 +1,19 @@
+const Navigation = () => {
+    return (
+    <nav className="container">
+        <div className="logo">
+          <img src="/images/brand_logo.png" alt="logo" />
+        </div>
+
+        <ul>
+          <li>Menu</li>
+          <li>Location</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+
+        <button>Login</button>
+    </nav>
+      );
+};
+export default Navigation;
